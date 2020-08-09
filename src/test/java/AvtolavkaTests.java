3626392c9d0ba5f77c9ed317e73255a28b81e76b
Vasenkov Stanislav <aanher@gmail.com>
2020-08-09 23:03:31 +0300

    Update AvtolavkaTests.java
    code review

--- src/test/java/AvtolavkaTests.java
+++ src/test/java/AvtolavkaTests.java
@@ -8,55 +8,54 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class AvtolavkaTests {
     @BeforeEach
     void closeModal() {
         open("https://avtolavka.net/");
-
-        $(".fancybox-item").click();//закрываем модалку
-
+        $(".fancybox-item").click(); // закрываем модалку
         $("html").shouldNotHave(text("Выбор офиса обслуживания"));
-
     }
 
     @Test
-    void openPageWithBatteryTest() {// открытие раздела с главной страницы
+    void openPageWithBatteryTest() { // открытие раздела с главной страницы
         $(byText("АКБ")).click();
 
-        Assertions.assertEquals(url(), "https://avtolavka.net/batteries_catalog"); //проверяем url
+        assertEquals(url(), "https://avtolavka.net/batteries_catalog");
     }
 
     @Test
-    void findFromFieldTest() {//поиск из строки поиска
+    void findItemFromFieldTest() { // поиск из строки поиска
         $("#pcode").setValue("грм").pressEnter();
 
         $("#searchResultsTable > tbody").shouldBe(text("Башмак цепи грм"));
     }
 
     @Test
-    void openAndClickFromMenyTest() {//переход в раздел из выпадающего меню
+    void openItemFromMenuTest() { // переход в раздел из выпадающего меню
         $$(".headCatalog.fr-dropdown-toggle").find(visible).click();
         $$(".fr-dropdown-menu")
                 .find(visible)
                 .$(byText("Компрессоры"))
                 .click();
 
-        $("html").shouldBe(text("Компрессор"));
+        $("html").shouldBe(text("Компрессор")); // todo fix test
     }
 
     @Test
-    void addInBasketTest() {
+    void addToCartTest() {
         $$(".headCatalog.fr-dropdown-toggle").find(visible).click();
         $$(".fr-dropdown-menu")
                 .find(visible)
                 .$(byText("Ароматизаторы"))
                 .click();
-        //Ищем товар и нажимаем на добавить в корзину
-        $("html").shouldBe(text("Ароматизатор"));
+        $("html").shouldBe(text("Ароматизатор")); // Ищем товар и нажимаем на добавить в корзину
 
-        $$(".fr-icon2-basket-2").find(visible).click();
+        $$(".fr-icon2-basket-2").find(visible).click(); // todo fix
         $$(".wCart").find(visible).click();
+        
         //проверка добавления товара в корзину
         $("#formTrash").shouldBe(text("Ароматизатор"));
 
@@ -66,9 +65,10 @@ public class AvtolavkaTests {
     void formAuthTest() {
         $(".loginLink").click();
 
-        String login = $("#login").getAttribute("placeholder");
+        String login = $("#login").getAttribute("placeholder"); // todo .shouldHave(attribute(“class”, “active checked”))
         String pass = $("#pass").getAttribute("placeholder");
         String btnGo = $("#go").getAttribute("value");
+        
         //проверяем наличие полей на поле Авторизация
         Assertions.assertEquals("Логин", login);
         Assertions.assertEquals("Пароль", pass);
